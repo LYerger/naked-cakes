29197c4b1190511c4cb42b03a92ac54d44356ef3
LYerger <lorraine.yerger@gmail.com>
2017-09-19 11:39:53 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    
+      <!-- Custom CSS -->
+    <link rel="stylesheet" href="index.css">
+  </head>
+    
+  <body>
+    <!-- Header-->
+    <a href="home.html"><img class="header-img" src="img/NakedCakesTestLogo.gif"></a>
+      
+    <!-- Navigation Bar -->
+    <div class="container">
+        <div class="topnav">
+            <ul id="ul-nav">
+                <li id="li-nav"><a href="home.html">Home</a></li>
+                <li id="li-nav"><a href="#">Menu</a></li>
+                <li id="li-nav"><a href="#">Gallery</a></li>
+                <li id="li-nav"><a href="#">About</a></li>
+                <li id="li-nav"><a href="place-order.html">Place Order</a></li>
+            </ul>
+            <hr>
+        </div>        
+        <p id="ready-to-order">Ready to place your order?</p>
+        
+        <p id="order-step">Step 1: Choose</p>
+ 
+    </div>
+      
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
